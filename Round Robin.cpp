eaf7494543463b239eaa9645b6c31f67a2e59786
xFSDev <126325621+xOniiChan@users.noreply.github.com>
2024-09-18 12:34:08 +0600

    Update Round Robin.cpp
    Initial Code Added

--- Round Robin.cpp
+++ Round Robin.cpp
@@ -1,1 +1,56 @@
+#include<iostream>
+using namespace std;
 
+struct process {
+int id;
+int AT,BT,TAT,WT,RT;
+sort(struct process p[],int n) {
+struct process temp;
+
+{
+for (int i=0;i<n-1;i++)
+  for(int j=0;j<n;j++) {
+if (p[i}.AT>p[j].AT){
+temp=p[i];
+p[i]=p[j];
+p[j]=temp;
+}
+  }
+}
+
+void RR(struct process p[p],int n; int TQ){
+  int CT=0;
+int completed=0;
+int ready[n];
+int front=0;
+int rear =0;
+
+for (int i=0;i<n;i++)
+{
+ready[rear]=i;
+rear++;
+}
+
+
+CT=p[0].AT;
+
+
+while(completed<n){
+int min_id=front=;
+front=(front+1)mod n;
+
+if (p[min_id].RT>0)
+  if(p[min_id].RT>TQ)
+    CT=CT+TQ;
+p[min_id].RT=p[min_id].RT-TQ;
+rear=(rear+1)mod n;
+ready[rear]=min_id;
+}
+
+else completed++;
+CT=CT+p[min_id].RT;
+p[min_id]-RT=0;
+front=(front+1)mod n;
+else {
+CT++;
+}
