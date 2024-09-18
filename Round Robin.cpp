35c25c4c9b0d5fec3ec69c5737d02f9fe53662cf
xFSDev <126325621+xOniiChan@users.noreply.github.com>
2024-09-18 13:01:48 +0600

    Update Round Robin.cpp
    Temp

--- Round Robin.cpp
+++ Round Robin.cpp
@@ -54,3 +54,126 @@ front=(front+1)mod n;
 p[min_id].TAT=CT-p[min_id].AT;
 p[min_id].WT=p[min_id].TAT-p[min_id].BT;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include <iostream>
+using namespace std;
+
+struct process {
+    int id;
+    int AT;
+    int BT;
+    int TAT;
+    int WT;
+    int RT;
+};
+
+void sort(process p[], int n) {
+    process temp;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (p[i].AT > p[j].AT) {
+                temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
+            }
+        }
+    }
+}
+
+void RR(process p[], int n, int TQ) {
+    int CT = 0;
+    int completed = 0;
+    int front = 0;
+    int rear = 0;
+    int ready[n];
+
+    for (int i = 0; i < n; i++) {
+        p[i].RT = p[i].BT;
+    }
+
+    for (int i = 0; i < n; i++) {
+        ready[rear++] = i;
+    }
+
+    CT = p[0].AT;
+
+    while (completed < n) {
+        int min_id = ready[front++];
+        if (front >= rear) {
+            front = 0;
+        }
+
+        if (p[min_id].RT > 0) {
+            if (p[min_id].RT > TQ) {
+                CT += TQ;
+                p[min_id].RT -= TQ;
+            } else {
+                CT += p[min_id].RT;
+                p[min_id].RT = 0;
+                completed++;
+                p[min_id].TAT = CT - p[min_id].AT;
+                p[min_id].WT = p[min_id].TAT - p[min_id].BT;
+            }
+            if (p[min_id].RT > 0) {
+                ready[rear++] = min_id;
+                if (rear >= n) {
+                    rear = 0;
+                }
+            }
+        }
+    }
+}
+
+void printProcesses(process p[], int n) {
+    cout << "Process ID\tArrival Time\tBurst Time\tTurnaround Time\tWaiting Time\n";
+    for (int i = 0; i < n; i++) {
+        cout << p[i].id << "\t\t" << p[i].AT << "\t\t" 
+             << p[i].BT << "\t\t" << p[i].TAT << "\t\t" << p[i].WT << "\n";
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of processes: ";
+    cin >> n;
+
+    process p[n];
+
+    for (int i = 0; i < n; i++) {
+        cout << "Enter Arrival Time and Burst Time for process " << (i + 1) << ": ";
+        cin >> p[i].AT >> p[i].BT;
+        p[i].id = i + 1;
+    }
+
+    int TQ;
+    cout << "Enter Time Quantum: ";
+    cin >> TQ;
+
+    sort(p, n);
+    RR(p, n, TQ);
+    printProcesses(p, n);
+
+    return 0;
+}
+
